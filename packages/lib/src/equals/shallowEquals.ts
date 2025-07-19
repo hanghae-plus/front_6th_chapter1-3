179425294d01337208311134d1682c7809a1949b
ure <uy23@naver.com>
2025-07-20 00:53:53 +0900

    feat: shallowEquals 구현
    - 타입 / null / array / obj 비교

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,3 +1,21 @@
-export const shallowEquals = (a: unknown, b: unknown) => {
+type AnonymousObj = { [key: string]: unknown };
+
+export const shallowEquals = (a: unknown, b: unknown): boolean => {
+  if (typeof a !== typeof b) return false;
+
+  if (a === null || b === null) return a === b;
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length) return false;
+    return a.every((item, index) => item === b[index]);
+  }
+
+  if (typeof a === "object" && typeof b === "object") {
+    const aKeys = Object.keys(a);
+    const bKeys = Object.keys(b);
+    if (aKeys.length !== bKeys.length) return false;
+    return aKeys.every((key) => (a as AnonymousObj)[key] === (b as AnonymousObj)[key]);
+  }
+
   return a === b;
 };
