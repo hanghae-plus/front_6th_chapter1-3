f7816824dadce32f96b9655d5230ff362567d53c
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-20 13:36:11 +0900

    feat: shallowEquals 구현
    Refs: #1

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,3 +1,41 @@
 export const shallowEquals = (a: unknown, b: unknown) => {
-  return a === b;
+  // https://github.com/JiHoon-0330/front_6th_chapter1-3/issues/1
+  if (Object.is(a, b)) {
+    return true;
+  }
+
+  if (a == null || b == null) {
+    return false;
+  }
+
+  if (isArray(a) && isArray(b)) {
+    return a.length === b.length && a.every((value, index) => value === b[index]);
+  }
+
+  if (isObject(a) && isObject(b)) {
+    const keysA = Object.keys(a);
+    const keysB = Object.keys(b);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    for (const key of keysA) {
+      if (!Object.is(a[key], b[key])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  return false;
 };
+
+function isArray(a: unknown): a is unknown[] {
+  return Array.isArray(a);
+}
+
+function isObject(a: unknown): a is Record<string, unknown> {
+  return typeof a === "object" && a !== null;
+}
