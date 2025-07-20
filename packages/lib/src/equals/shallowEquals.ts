157f80dbe004c3ba0d9c955a80623eb71b23ec50
yuhyeon99 <jujini31@gmail.com>
2025-07-21 00:45:37 +0900

    feat: 객체의 얕은(shallow) 비교 및 깊은(deep) 비교 유틸 함수 구현
    - Object.is를 활용한 얕은 비교 함수(shallowEquals) 구현
      - 객체의 직접 속성과 각 속성의 값이 동일한지 비교
    - 재귀 호출을 통한 깊은 비교 함수(deepEquals) 구현
      - 중첩된 객체 및 배열 구조까지 깊이 비교 가능
    - 타입 안전성을 위해 unknown 타입에 대한 단언 처리 적용

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,3 +1,28 @@
 export const shallowEquals = (a: unknown, b: unknown) => {
-  return a === b;
+  if (Object.is(a, b)) {
+    return true;
+  }
+
+  if (typeof a !== "object" || a === null || typeof b !== "object" || b === null) {
+    return false;
+  }
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (let i = 0; i < keysA.length; i++) {
+    const currentKey = keysA[i];
+    if (
+      !Object.prototype.hasOwnProperty.call(b, currentKey) ||
+      !Object.is((a as Record<string, unknown>)[currentKey], (b as Record<string, unknown>)[currentKey])
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 };
