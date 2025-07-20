5eb891bfc35a8a862ccf88a8e34788bac35470c7
chan9yu <dev.cgyeo@gmail.com>
2025-07-20 22:03:08 +0900

    feat: useRef 구현

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -1,4 +1,14 @@
-export function useRef<T>(initialValue: T): { current: T } {
-  // useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+import { useState } from "react";
+
+type RefObject<T> = {
+  current: T;
+};
+
+/**
+ * React의 useRef 훅을 구현
+ * - useState를 이용해서 리렌더링 간에 동일한 참조를 유지하는 ref 객체를 생성
+ */
+export function useRef<T>(initialValue: T) {
+  const [refObject] = useState<RefObject<T>>({ current: initialValue });
+  return refObject;
 }
