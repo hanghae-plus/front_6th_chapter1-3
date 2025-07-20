c8facec38b453a3033a5dd45c6c600bfa98314c0
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-20 17:00:55 +0900

    feat: useAutoCallback 구현

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -3,5 +3,9 @@ import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
 
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const ref = useRef<T>(fn);
+  // 리렌더링에서 함수를 다시 할당하기 위해
+  ref.current = fn;
+
+  return useCallback<T>(((...args) => ref.current(...args)) as T, []);
 };
