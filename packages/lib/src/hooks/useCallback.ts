72d61583033a420722cf31d5e5e8f39b8a4e6174
ure <uy23@naver.com>
2025-07-20 23:42:13 +0900

    feat: useCallback 구현

--- packages/lib/src/hooks/useCallback.ts
+++ packages/lib/src/hooks/useCallback.ts
@@ -1,7 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
 import type { DependencyList } from "react";
+import { useRef } from "./useRef";
+import { shallowEquals } from "../equals";
 
+// React.useCallback과 동일하게 extends Function이지만, lint 이슈로 인해 disable 주석 추가
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export function useCallback<T extends Function>(factory: T, _deps: DependencyList) {
-  // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  const prevDepsRef = useRef<DependencyList | null>(null);
+  const factoryRef = useRef<T | undefined>(undefined);
+
+  if (prevDepsRef.current === null || !shallowEquals(prevDepsRef.current, _deps)) {
+    prevDepsRef.current = _deps;
+    factoryRef.current = factory;
+  }
+
+  return factoryRef.current as T;
 }
