4997185a6e424e98fe47d178d752e0780e5613c0
ure <uy23@naver.com>
2025-07-21 01:17:29 +0900

    feat: useAutoCallback 구현

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -1,7 +1,18 @@
+/**
+ * useAutoCallback 특징
+ * 의존성이 변경되어도 참조가 변경되지 않으면서도 최신 값을 참조하는 안정적인 버전의 useCallback
+ * 의존성을 따로 추가 하지 않아도 됨
+ */
 import type { AnyFunction } from "../types";
-// import { useCallback } from "./useCallback";
-// import { useRef } from "./useRef";
+import { useCallback } from "./useCallback";
+import { useRef } from "./useRef";
 
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const latestFnRef = useRef<T | undefined>(undefined);
+  latestFnRef.current = fn;
+  const staticFn = useCallback((...args: Parameters<T>): ReturnType<T> => {
+    if (!latestFnRef.current) throw new Error("최신 함수는 항상 존재해야 합니다.");
+    return latestFnRef.current(...args);
+  }, []);
+  return staticFn as T;
 };
