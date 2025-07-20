5800f8dd12b1abcd4f830a71ae73c138ecddeefa
ure <uy23@naver.com>
2025-07-20 20:32:17 +0900

    feat: ref구현 (useState를 사용하는 버전)

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export function useRef<T>(initialValue: T): { current: T } {
   // useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  // 리렌더 시 값을 유지하면서도 불변성 유지하지 못하게 set함수 대신 직접 초기화 하도록 구현됨.
+  const [ref] = useState(() => ({ current: initialValue }));
+  return ref;
 }
