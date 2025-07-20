279080c14298c8dd8bdd4126349c6bcfad84364b
chan9yu <dev.cgyeo@gmail.com>
2025-07-20 23:53:01 +0900

    refactor: useRef 주석 정리

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -4,11 +4,8 @@ type RefObject<T> = {
   current: T;
 };
 
-/**
- * React의 useRef 훅을 구현
- * - useState를 이용해서 리렌더링 간에 동일한 참조를 유지하는 ref 객체를 생성
- */
 export function useRef<T>(initialValue: T) {
+  // useState를 이용해서 리렌더링 간에 동일한 참조를 유지하는 ref 객체를 생성
   const [refObject] = useState<RefObject<T>>({ current: initialValue });
   return refObject;
 }
