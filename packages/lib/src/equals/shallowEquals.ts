6fb07e9a7d7deeef0ec286e735122fccc4dc0ff6
eveneul <nebulas9724@gmail.com>
2025-07-20 19:58:27 +0900

    feat: shallowEquals 함수 개선 - 객체 비교 로직 추가

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,3 +1,29 @@
-export const shallowEquals = (a: unknown, b: unknown) => {
-  return a === b;
+export const shallowEquals = (objA: unknown, objB: unknown): boolean => {
+  // 객체가 다르면 false 리턴
+  if (typeof objA !== "object" || objA === null || typeof objB !== "object" || objB === null) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  // 길이가 다르면 false 리턴
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  // A 키를 기준으로 B에 같은 키가 있는지, 그리고 그 값이 있는지 확인
+  // Object.prototype.hasOwnProperty.call() => 객체가 해당 속성을 직접 소유하고 있는지 확인
+  for (let i = 0; i < keysA.length; i++) {
+    const currentKey = keysA[i];
+
+    if (
+      !Object.prototype.hasOwnProperty.call(objB, currentKey) ||
+      !Object.is((objA as Record<string, unknown>)[currentKey], (objB as Record<string, unknown>)[currentKey])
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 };
