3da8a2329e936ee66b37cb3b20f55111d6d0d2e1
changjun <ckdwns9121@naver.com>
2025-07-21 22:31:15 +0900

    useCallback, useMemo 구현

--- packages/lib/src/hooks/useMemo.ts
+++ packages/lib/src/hooks/useMemo.ts
@@ -1,8 +1,34 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { DependencyList } from "react";
+import { useRef } from "./useRef";
 import { shallowEquals } from "../equals";
 
-export function useMemo<T>(factory: () => T, _deps: DependencyList, _equals = shallowEquals): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+type MemoizedState<T> = {
+  value: T;
+  deps: DependencyList;
+};
+
+function hasDependenciesChanged<T>(
+  prev: MemoizedState<T> | null,
+  deps: DependencyList,
+  equals: (a: DependencyList, b: DependencyList) => boolean,
+): boolean {
+  return prev === null || !equals(prev.deps, deps);
+}
+
+function computeMemoized<T>(
+  prev: MemoizedState<T> | null,
+  factory: () => T,
+  deps: DependencyList,
+  equals: (a: DependencyList, b: DependencyList) => boolean,
+): MemoizedState<T> {
+  if (hasDependenciesChanged(prev, deps, equals)) {
+    return { value: factory(), deps };
+  }
+  return prev!;
+}
+
+export function useMemo<T>(factory: () => T, deps: DependencyList, equals = shallowEquals): T {
+  const ref = useRef<MemoizedState<T> | null>(null);
+  ref.current = computeMemoized(ref.current, factory, deps, equals);
+  return ref.current.value;
 }
