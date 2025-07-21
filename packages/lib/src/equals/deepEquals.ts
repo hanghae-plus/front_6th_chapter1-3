3ad65c8a956c42db23412a559c88d31d2426f463
BangDori <qudwns8865@naver.com>
2025-07-21 15:43:45 +0900

    feat: deepEquals
    - isObject 타입 가드 활용
    - 값이 객체인 경우 재귀적으로 비교

--- packages/lib/src/equals/deepEquals.ts
+++ packages/lib/src/equals/deepEquals.ts
@@ -1,3 +1,32 @@
-export const deepEquals = (a: unknown, b: unknown) => {
+import { isObject } from "../utils/type-guard";
+
+/**
+ * 깊은 비교를 통해 두 값이 같은지 비교하는 함수
+ * @param a 비교할 값 1
+ * @param b 비교할 값 2
+ * @returns 깊은 비교 결과 (같으면 true, 다르면 false)
+ */
+export const deepEquals = (a: unknown, b: unknown): boolean => {
+  if (a === b) return true;
+
+  if (isObject(a) && isObject(b)) {
+    const aKeys = Object.keys(a);
+    const bKeys = Object.keys(b);
+
+    if (aKeys.length !== bKeys.length) return false;
+
+    const uniqueKeys = new Set([...aKeys, ...bKeys]);
+
+    for (const key of uniqueKeys) {
+      if (isObject(a[key]) && isObject(b[key])) {
+        return deepEquals(a[key], b[key]);
+      }
+
+      if (a[key] !== b[key]) return false;
+    }
+
+    return true;
+  }
+
   return a === b;
 };
