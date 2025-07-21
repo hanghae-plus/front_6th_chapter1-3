bb47b7cbbe57b4a3d2158f80d5c861fc9b6fb788
yong <yong@thecodit.com>
2025-07-21 22:47:54 +0900

    feat: deepEquals 비교 연산을 Object.is로 변경

--- packages/lib/src/equals/deepEquals.ts
+++ packages/lib/src/equals/deepEquals.ts
@@ -1,33 +1,21 @@
 import { getObjectKeys, isArray, isObject, isPrimitive } from "./utils";
 
-export const deepEquals = (a: unknown, b: unknown) => {
-  if (typeof a !== typeof b) return false;
+const comparePrimitive = (a: unknown, b: unknown) => Object.is(a, b);
 
+export const deepEquals = (a: unknown, b: unknown): boolean => {
   if (isPrimitive(a) && isPrimitive(b)) {
-    return a === b;
+    return comparePrimitive(a, b);
   }
 
   if (isArray(a) && isArray(b)) {
-    if (a.length !== b.length) return false;
-
-    for (let i = 0; i < a.length; i++) {
-      if (!deepEquals(a[i], b[i])) return false;
-    }
-
-    return true;
+    return a.length === b.length && a.every((item, index) => deepEquals(item, b[index]));
   }
 
   if (isObject(a) && isObject(b)) {
     const keysA = getObjectKeys(a);
     const keysB = getObjectKeys(b);
 
-    if (keysA.length !== keysB.length) return false;
-
-    for (const key of keysA) {
-      if (!deepEquals(a[key], b[key])) return false;
-    }
-
-    return true;
+    return keysA.length === keysB.length && keysA.every((key) => deepEquals(a[key], b[key]));
   }
 
   return a === b;
