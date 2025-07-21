66fe47b5d045e6e423ac99377694ef63276e1a51
yong <yong@thecodit.com>
2025-07-22 00:56:29 +0900

    feat: memo, deepMemo 구현

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,21 @@
-import { type FunctionComponent } from "react";
+import { useRef, type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  const MemoizedComponent = (props: P) => {
+    const prevPropsRef = useRef<P | null>(null);
+    const prevResultRef = useRef<ReturnType<typeof Component> | null>(null);
+
+    if (prevPropsRef.current && equals(prevPropsRef.current, props)) {
+      return prevResultRef.current!;
+    }
+
+    const result = Component(props);
+    prevPropsRef.current = props;
+    prevResultRef.current = result;
+
+    return result;
+  };
+
+  return MemoizedComponent;
 }
