2d9efaa8a4f3f0975b8d2d0f55b8f8f2ca68ad94
changjun <ckdwns9121@naver.com>
2025-07-22 01:02:44 +0900

    memo, useAutoCallback 구현

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,18 @@
-import { type FunctionComponent } from "react";
+import { createElement, type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks/useRef";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  return function MemoizedComponent(props: P) {
+    const prevProps = useRef<P | null>(null);
+    const prevComponent = useRef<React.ReactNode | null>(null);
+
+    if (prevProps.current && equals(prevProps.current, props)) {
+      return prevComponent.current!;
+    }
+
+    prevProps.current = props;
+    prevComponent.current = createElement(Component, props);
+    return prevComponent.current! as React.ReactElement;
+  };
 }
