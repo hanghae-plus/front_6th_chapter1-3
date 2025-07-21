2d342d8620d16ea0cd5808e31571d49c01ce5a73
Yuyeol <uy23@naver.com>
2025-07-21 17:51:12 +0900

    feat: 스토어구현
    - useShallowSelector
    - useStore

--- packages/lib/src/hooks/useShallowSelector.ts
+++ packages/lib/src/hooks/useShallowSelector.ts
@@ -1,9 +1,17 @@
-// import { useRef } from "react";
-// import { shallowEquals } from "../equals";
+import { shallowEquals } from "../equals";
+import { useRef } from "./useRef";
 
+// 셀렉트 값 S를 지정하지않으면 전체 state T를 반환
 type Selector<T, S = T> = (state: T) => S;
 
-export const useShallowSelector = <T, S = T>(selector: Selector<T, S>) => {
-  // 이전 상태를 저장하고, shallowEquals를 사용하여 상태가 변경되었는지 확인하는 훅을 구현합니다.
-  return (state: T): S => selector(state);
+export const useShallowSelector = <T, S = T>(selector: Selector<T, S>): Selector<T, S> => {
+  const prevResultRef = useRef<S | undefined>(undefined);
+
+  const shallowSelector = (state: T): S => {
+    const result = selector(state);
+    if (!shallowEquals(prevResultRef.current, result)) prevResultRef.current = result;
+    return prevResultRef.current as S;
+  };
+
+  return shallowSelector;
 };
