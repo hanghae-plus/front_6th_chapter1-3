b33045de6193539ca1ff20ce1acc8a1e263096a6
bebusl <bebus1998@naver.com>
2025-07-21 21:59:46 +0900

    feat: useAutoCallback 정의

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -3,5 +3,9 @@ import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
 
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const ref = useRef(fn);
+  ref.current = fn; //함수바뀔 때마다 넣어줌
+
+  const memoizedCallback = useCallback(() => ref.current(), []); // ref를 참조하므로 useCallback은 재계산 일어나지 않음.
+  return memoizedCallback as T;
 };
