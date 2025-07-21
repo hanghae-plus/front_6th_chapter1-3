7ade6f77c323762b3f641d5a3772a1df58218550
Angie Lee <angielxx94@gmail.com>
2025-07-21 23:38:17 +0900

    feat: createObserver 수정 및 useStorage 구현

--- packages/lib/src/hooks/useStorage.ts
+++ packages/lib/src/hooks/useStorage.ts
@@ -1,10 +1,32 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useSyncExternalStore } from "react";
 import type { createStorage } from "../createStorage";
 
 type Storage<T> = ReturnType<typeof createStorage<T>>;
 
+/**
+ * 저장소를 사용하는 훅
+ *
+ * @param storage - 저장소 핸들러
+ * @returns 저장소의 상태
+ */
 export const useStorage = <T>(storage: Storage<T>) => {
-  // useSyncExternalStore를 사용해서 storage의 상태를 구독하고 가져오는 훅을 구현해보세요.
-  return storage.get();
+  /**
+   * useSyncExternalStore<T | null>
+   * (subscribe: (onStoreChange: () => void) => () => void,
+   * getSnapshot: () => T | null,
+   * getServerSnapshot?: (() => T | null) | undefined): T | null
+   */
+  const snapshot = useSyncExternalStore(subscribe, getSnapshot);
+
+  function subscribe(onStoreChange: () => void): () => void {
+    const unsubscribe = storage.subscribe(onStoreChange);
+
+    return () => unsubscribe(onStoreChange);
+  }
+
+  function getSnapshot(): T | null {
+    return storage.get();
+  }
+
+  return snapshot;
 };
