c0354fd7a2d675e3f01c073b0f1a016567a95299
Amelia-Shin <gmldnjs0402@naver.com>
2025-07-22 03:45:54 +0900

    feat: useAutoCallback 추가

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -3,5 +3,10 @@ import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
 
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const ref = useRef<T>(fn);
+
+  // 이후 렌더마다 최신값으로 업데이트
+  ref.current = fn;
+
+  return useCallback(() => ref.current(), []) as T;
 };
