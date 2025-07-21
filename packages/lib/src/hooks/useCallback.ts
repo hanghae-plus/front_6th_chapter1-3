ec04970c3181b36ab2c17388304527a7259f3ffa
yong <yong@thecodit.com>
2025-07-21 23:05:20 +0900

    fix: useMemo dependency 배열에 factory 제거

--- packages/lib/src/hooks/useCallback.ts
+++ packages/lib/src/hooks/useCallback.ts
@@ -2,7 +2,7 @@ import type { DependencyList } from "react";
 import { useMemo } from "./useMemo";
 
 export function useCallback<T extends (...args: unknown[]) => unknown>(factory: T, deps: DependencyList) {
-  const memoized = useMemo(() => factory, [...deps, factory]);
+  const memoized = useMemo(() => factory, [...deps]);
 
   return memoized;
 }
