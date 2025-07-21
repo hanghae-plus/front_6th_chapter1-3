1126ccc26194884eef0f5f95c08a447e91c0e749
BangDori <qudwns8865@naver.com>
2025-07-21 16:24:56 +0900

    feat: useRef
    - useState 지연 초기화를 적용

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
+/**
+ * 참조 값을 관리하는 훅
+ * @param initialValue 초기 값
+ * @returns 참조 값
+ */
 export function useRef<T>(initialValue: T): { current: T } {
-  // useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  // useState의 지연 초기화 기능을 활용
+  const [ref] = useState<{ current: T }>(() => ({ current: initialValue }));
+
+  return ref;
 }
