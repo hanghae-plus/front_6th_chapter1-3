172d0331aba15fdec481d761b5a4038bd6766963
Amelia-Shin <gmldnjs0402@naver.com>
2025-07-22 04:40:29 +0900

    fix: 배열의 길이가 다른 경우 예외처리 추가

--- packages/lib/src/equals/deepEquals.ts
+++ packages/lib/src/equals/deepEquals.ts
@@ -1,8 +1,10 @@
 export const deepEquals = (a: unknown, b: unknown) => {
   // 1. 기본 타입이거나 null인 경우 처리
-  
-  if (a === b) { return true; }
-  
+
+  if (a === b) {
+    return true;
+  }
+
   if (typeof a !== "object" || typeof b !== "object" || a === null || b === null) {
     return false;
   }
@@ -10,15 +12,22 @@ export const deepEquals = (a: unknown, b: unknown) => {
   //    - 배열인지 확인
   //    - 객체의 키 개수가 다른 경우 처리
   //    - 재귀적으로 각 속성에 대해 deepEquals 호출
-  if (Array.isArray(a) && Array.isArray(b)) { 
-    for (let i = 0; i < a.length; i ++) {
-      if (!deepEquals(a[i], b[i])) { return false; }
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length) {
+      return false;
+    }
+    for (let i = 0; i < a.length; i++) {
+      if (!deepEquals(a[i], b[i])) {
+        return false;
+      }
     }
     return true;
   } else {
     // Object.keys(), Object.entries() 등으로 키를 순회
-    if (Object.keys(a).length !== Object.keys(b).length) { return false; }
-   
-    return Object.keys(a).every(key => deepEquals(a[key], b[key]));
+    if (Object.keys(a).length !== Object.keys(b).length) {
+      return false;
+    }
+
+    return Object.keys(a).every((key) => deepEquals(a[key], b[key]));
   }
 };
