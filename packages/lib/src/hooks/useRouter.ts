61757d4962ea7e89b58da81d74199c8a59ccc2f0
eveneul <nebulas9724@gmail.com>
2025-07-22 01:02:25 +0900

    feat: useRouter 훅 구현

--- packages/lib/src/hooks/useRouter.ts
+++ packages/lib/src/hooks/useRouter.ts
@@ -8,5 +8,7 @@ const defaultSelector = <T, S = T>(state: T) => state as unknown as S;
 export const useRouter = <T extends RouterInstance<AnyFunction>, S>(router: T, selector = defaultSelector<T, S>) => {
   // useSyncExternalStore를 사용하여 router의 상태를 구독하고 가져오는 훅을 구현합니다.
   const shallowSelector = useShallowSelector(selector);
-  return shallowSelector(router);
+  // useSyncExternalStore 두 번째 인자(현재상태)는 "전체 상태"를 반환해야 함
+  const state = useSyncExternalStore(router.subscribe, () => shallowSelector(router));
+  return state;
 };
