9a5696ff082b84bd5a68ca0356b3e2703b2d5e72
eveneul <nebulas9724@gmail.com>
2025-07-22 00:42:41 +0900

    feat: useStorage 훅에서 상태 구독 로직을 useSyncExternalStore를 사용하여 구현

--- packages/lib/src/hooks/useStorage.ts
+++ packages/lib/src/hooks/useStorage.ts
@@ -5,5 +5,11 @@ type Storage<T> = ReturnType<typeof createStorage<T>>;
 
 export const useStorage = <T>(storage: Storage<T>) => {
   // useSyncExternalStore를 사용해서 storage의 상태를 구독하고 가져오는 훅을 구현해보세요.
-  return storage.get();
+
+  // storage는 createStorage 함수가 반환하는 객체
+  // get, set, reset, subscribe 리턴
+
+  const state = useSyncExternalStore(storage.subscribe, () => storage.get());
+
+  return state;
 };
