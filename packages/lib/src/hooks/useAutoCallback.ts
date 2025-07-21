c1efb5357d22d99e9f1592022095602551392f88
esoby <esoby104@gmail.com>
2025-07-22 00:28:56 +0900

    feat: useAutoCallback 작성

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -2,6 +2,17 @@ import type { AnyFunction } from "../types";
 import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
 
+// useCallback과 useRef를 이용하여 useAutoCallback
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  // - 콜백함수가 참조하는 값은 항상 렌더링 시점에 최신화
+  // - 항상 동일한 참조를 유지
+  const fnRef = useRef(fn);
+
+  fnRef.current = fn;
+
+  const memoizedCallback = useCallback((...args: Parameters<T>): T => {
+    return fnRef.current(...args) as T;
+  }, []);
+
+  return memoizedCallback as T;
 };
