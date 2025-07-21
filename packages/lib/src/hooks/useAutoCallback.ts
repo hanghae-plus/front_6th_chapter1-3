9d3ed404c46db442658da0daa5840be55c629210
bebusl <bebus1998@naver.com>
2025-07-22 01:45:11 +0900

    fix: useAutoCallback에서 props받게 수정

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -6,6 +6,6 @@ export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
   const ref = useRef(fn);
   ref.current = fn; //함수바뀔 때마다 넣어줌
 
-  const memoizedCallback = useCallback(() => ref.current(), []); // ref를 참조하므로 useCallback은 재계산 일어나지 않음.
+  const memoizedCallback = useCallback((props) => ref.current(props), []); // ref를 참조하므로 useCallback은 재계산 일어나지 않음.
   return memoizedCallback as T;
 };
