51d61d7597799c2c515760bfee46c614a6f1c7f4
bebusl <bebus1998@naver.com>
2025-07-21 23:07:11 +0900

    chore: gh-pages 배포 관련 설정

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,9 +1,19 @@
 import react from "@vitejs/plugin-react-oxc";
 import { createViteConfig } from "../../createViteConfig";
+import { resolve } from "path";
+import fs from "fs";
 
 const base: string = process.env.NODE_ENV === "production" ? "/front_6th_chapter1-3/" : "";
 
 export default createViteConfig({
   base,
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "copy-index-to-404",
+      closeBundle() {
+        fs.copyFileSync(resolve(__dirname, "dist/index.html"), resolve(__dirname, "dist/404.html"));
+      },
+    },
+  ],
 });
