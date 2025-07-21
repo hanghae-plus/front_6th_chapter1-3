7f2441ffe1b5f7f2f084670143b1644d56245d18
yong <yong@thecodit.com>
2025-07-22 00:18:23 +0900

    feat: useAutoCallback 구현

--- packages/lib/src/hooks/useCallback.ts
+++ packages/lib/src/hooks/useCallback.ts
@@ -1,8 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import type { DependencyList } from "react";
 import { useMemo } from "./useMemo";
+import type { AnyFunction } from "../types";
 
-export function useCallback<T extends (...args: unknown[]) => unknown>(factory: T, deps: DependencyList) {
+export function useCallback<T extends AnyFunction>(factory: T, deps: DependencyList): T {
   const memoized = useMemo(() => factory, [...deps]);
 
   return memoized;
