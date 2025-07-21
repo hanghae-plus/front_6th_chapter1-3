96c6e6c75efec78f04e870744b79671fcf7da6ef
bebusl <bebus1998@naver.com>
2025-07-21 22:54:55 +0900

    feat: hoc memo 정의

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,24 @@
-import { type FunctionComponent } from "react";
+import { type FunctionComponent, type ReactNode } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  return function MemoizedComponent(props: P) {
+    const propsRef = useRef<P | null>(null);
+    const memoizedComponent = useRef<ReactNode | null>(null);
+
+    const cachedRender: FunctionComponent<P> = (props) => {
+      if (!propsRef.current || !equals(propsRef.current, props)) {
+        const renderedComponent = Component(props);
+        propsRef.current = props;
+        memoizedComponent.current = renderedComponent as ReactNode;
+
+        return renderedComponent;
+      }
+
+      return memoizedComponent.current;
+    };
+
+    return cachedRender(props);
+  };
 }
