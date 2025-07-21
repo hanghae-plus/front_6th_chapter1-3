1e41ca7217f02970869dbc1e2802748cf15d5b89
yong <yong@thecodit.com>
2025-07-21 23:35:04 +0900

    feat: useShallowState 구현

--- packages/lib/src/equals/utils.ts
+++ packages/lib/src/equals/utils.ts
@@ -7,3 +7,8 @@ export const isObject = (value: unknown): value is object => typeof value === "o
 export const isPrimitive = (value: unknown): value is Primitive => typeof value !== "object" || value === null;
 
 export const getObjectKeys = <T extends object>(o: T): (keyof T)[] => Object.keys(o) as (keyof T)[];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isFunction(value: any): value is (...args: any[]) => any {
+  return typeof value === "function";
+}
