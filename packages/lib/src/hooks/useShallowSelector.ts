94f7e5ee0b5e79a0e7396fdfc54c4c42e555357b
bebusl <bebus1998@naver.com>
2025-07-22 01:03:03 +0900

    feat: useStore 구현

--- packages/lib/src/hooks/useShallowSelector.ts
+++ packages/lib/src/hooks/useShallowSelector.ts
@@ -4,6 +4,20 @@ import { shallowEquals } from "../equals";
 type Selector<T, S = T> = (state: T) => S;
 
 export const useShallowSelector = <T, S = T>(selector: Selector<T, S>) => {
+  const _state = useRef<S | null>(null);
+
   // 이전 상태를 저장하고, shallowEquals를 사용하여 상태가 변경되었는지 확인하는 훅을 구현합니다.
-  return (state: T): S => selector(state);
+  /**@todo 이름 갱장히 애매. 고쳐주세요 */
+  const cachedSelector = (state: T): S => {
+    const selected = selector(state);
+
+    if (_state.current && shallowEquals(_state.current, selected)) {
+      return _state.current;
+    }
+
+    _state.current = selector(state);
+    return _state.current;
+  };
+
+  return cachedSelector;
 };
