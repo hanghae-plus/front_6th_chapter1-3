26c684ed15b95db65969959401e86db6c7776bf4
areumH <dkfmal0109@gmail.com>
2025-07-22 03:36:37 +0900

    feat: useMemo 함수 작성

--- packages/lib/src/hooks/useMemo.ts
+++ packages/lib/src/hooks/useMemo.ts
@@ -1,8 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { DependencyList } from "react";
+import { useRef } from "./useRef";
 import { shallowEquals } from "../equals";
 
+// factory - 계산할 함수
+// _deps - 의존성 배열
+// _equals - 의존성 배열 비교 함수
+// useRef - 저장된 배열과 새로운 배열을 비교
 export function useMemo<T>(factory: () => T, _deps: DependencyList, _equals = shallowEquals): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  const ref = useRef<{ deps: DependencyList; currentValue: T | undefined }>({
+    deps: [],
+    currentValue: undefined,
+  });
+
+  if (_equals(ref.current.deps, _deps)) {
+    return ref.current.currentValue as T;
+  }
+
+  const nextValue = factory();
+  ref.current.deps = _deps;
+  ref.current.currentValue = nextValue;
+
+  return nextValue;
 }
