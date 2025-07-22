83eb2b4548c8dbc1d8f5dece18e408124f1a8ad3
areumH <dkfmal0109@gmail.com>
2025-07-22 18:10:59 +0900

    feat: memo 함수 컴포넌트 작성

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,25 @@
 import { type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks";
 
+// props로 전달하는 값이 변경되어야 리렌더링
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  const MemoComponent = (props: P) => {
+    const componentRef = useRef<unknown | null>(null);
+    const propsRef = useRef<P | null>(null);
+
+    // 최초 렌더링이거나 props 값이 바뀜
+    if (!componentRef.current || !equals(propsRef.current, props)) {
+      const newComponent = Component(props);
+
+      componentRef.current = newComponent;
+      propsRef.current = props;
+
+      return newComponent;
+    }
+
+    return componentRef.current;
+  };
+
+  return MemoComponent;
 }
