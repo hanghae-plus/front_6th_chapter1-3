a1ad49955870837fde673f5062191b6d2994822e
YeongseoYoon <l112xpla@gmail.com>
2025-07-22 23:55:58 +0900

    refactor: createObserver 수정

--- packages/lib/src/createObserver.ts
+++ packages/lib/src/createObserver.ts
@@ -4,14 +4,13 @@ export const createObserver = () => {
   const listeners = new Set<Listener>();
 
   // useSyncExternalStore 에서 활용할 수 있도록 subscribe 함수를 수정합니다.
-  const subscribe = (fn: Listener) => {
-    listeners.add(fn);
-  };
+  const subscribe = (listener: Listener): (() => void) => {
+    listeners.add(listener);
 
-  const unsubscribe = (fn: Listener) => {
-    listeners.delete(fn);
+    return () => {
+      listeners.delete(listener);
+    };
   };
-
   const notify = () => listeners.forEach((listener) => listener());
 
   return { subscribe, notify };
