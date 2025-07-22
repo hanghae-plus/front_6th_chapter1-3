fef394e945ca2ac48c2b3db0bc1ae5cd9ad4020a
JHeeJinDev <zqewe12@gmail.com>
2025-07-22 18:59:23 +0900

    feat:"useAutoCallback"구현

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -3,5 +3,11 @@ import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
 
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const ref = useRef<T>(fn);
+
+  ref.current = fn;
+
+  const autoCallback = useCallback((...args: Parameters<T>) => ref.current!(...args), []);
+
+  return autoCallback as T;
 };
