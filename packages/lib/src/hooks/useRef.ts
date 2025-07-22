19a723fd47b1e9c94d8390d228b316a2f67b3b5f
YeongseoYoon <l112xpla@gmail.com>
2025-07-22 23:45:26 +0900

    feat: useRef 타입을 오버로딩하여 수정

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -1,8 +1,12 @@
 import { useState } from "react";
 
-export function useRef<T>(initialValue: T): { current: T } {
-  // useState를 이용해서 만들어보세요.
-  const [ref] = useState<{ current: T }>({ current: initialValue });
+export function useRef<T>(initialValue: T): { current: T };
+export function useRef<T>(initialValue: T | null): { current: T | null };
+export function useRef<T = undefined>(): { current: T | undefined };
+export function useRef<T>(initialValue?: T): { current: T | undefined } {
+  const [ref] = useState<{ current: T | undefined }>(() => ({
+    current: initialValue,
+  }));
 
   return ref;
 }
