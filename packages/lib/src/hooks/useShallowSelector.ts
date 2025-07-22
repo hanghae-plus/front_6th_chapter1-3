7285a464693c7b75e29344d267793b34fdd27a8f
Angie Lee <angielxx94@gmail.com>
2025-07-22 23:19:30 +0900

    feat: useStore, useShallowSelector 1차 구현 - 이전 상태를 useStore의 getSnapshot 내부에서 계산하고 비교

--- packages/lib/src/hooks/useShallowSelector.ts
+++ packages/lib/src/hooks/useShallowSelector.ts
@@ -1,10 +1,26 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useRef } from "react";
-import { shallowEquals } from "../equals";
+export type Selector<T, S = T> = (state: T) => S;
 
-type Selector<T, S = T> = (state: T) => S;
-
+/**
+ * 스토어의 상태를 선택하는 훅
+ *
+ * @param selector - 스토어의 상태를 선택하는 함수 ex. (state) => state.count)
+ * @returns selector 함수를 적용한 상태
+ */
 export const useShallowSelector = <T, S = T>(selector: Selector<T, S>) => {
   // 이전 상태를 저장하고, shallowEquals를 사용하여 상태가 변경되었는지 확인하는 훅을 구현합니다.
+  // const prevState = useRef<S | null>(null);
+
   return (state: T): S => selector(state);
+
+  // return (state: T): S => {
+  //   if (prevState.current === null) {
+  //     prevState.current = selector(state);
+  //   }
+
+  //   if (!shallowEquals(prevState.current, selector(state))) {
+  //     prevState.current = selector(state);
+  //   }
+
+  //   return prevState.current;
+  // };
 };
--- packages/lib/src/hooks/useStore.ts
+++ packages/lib/src/hooks/useStore.ts
@@ -1,14 +1,46 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import type { createStore } from "../createStore";
 import { useSyncExternalStore } from "react";
+import type { createStore } from "../createStore";
+import type { Selector } from "../types";
 import { useShallowSelector } from "./useShallowSelector";
+import { shallowEquals } from "../equals";
+import { useRef } from "./useRef";
 
 type Store<T> = ReturnType<typeof createStore<T>>;
 
 const defaultSelector = <T, S = T>(state: T) => state as unknown as S;
 
+/**
+ * 스토어를 사용하는 훅
+ *
+ * @param store - 스토어 핸들러
+ * @param selector - 스토어의 상태를 선택하는 함수 ex. (state) => state.count)
+ * @returns 선택된 스토어의 상태
+ */
 export const useStore = <T, S = T>(store: Store<T>, selector: (state: T) => S = defaultSelector<T, S>) => {
-  // useSyncExternalStore와 useShallowSelector를 사용해서 store의 상태를 구독하고 가져오는 훅을 구현해보세요.
-  const shallowSelector = useShallowSelector(selector);
+  const prevSelectedState = useRef<S | undefined>(undefined);
+
+  // ? 얘가 하는 일이 대체 뭐야? 왜 필요한거지? 반환값도 안 쓰면서?
+  useSyncExternalStore(subscribe, getSnapshot);
+
+  // 스토어를 구독하고 해지함수를 반환하는 함수
+  function subscribe(onStoreChange: () => void): () => void {
+    const unsubscribe = store.subscribe(onStoreChange);
+    return () => unsubscribe(onStoreChange);
+  }
+
+  // 스토어의 상태를 가져오는 함수
+  function getSnapshot() {
+    const state = store.getState();
+    const selected = selector(state);
+
+    if (prevSelectedState.current === undefined || !shallowEquals(prevSelectedState.current, selected)) {
+      prevSelectedState.current = selected;
+    }
+
+    return prevSelectedState.current;
+  }
+
+  const shallowSelector: Selector<T, S> = useShallowSelector(selector);
+
   return shallowSelector(store.getState());
 };
