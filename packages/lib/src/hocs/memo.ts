05493407b742adcb7935b987ea0e8f751fd0414a
Hwirin-Kim <kimhuirin@gmail.com>
2025-07-22 21:38:35 +0900

    feat : HOC memo 완성

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,20 @@
 import { type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  // 훅 사용을 위해 컴포넌트 생성
+  const MemoizedComponent = (props: P) => {
+    // 이전 프롭스와 리턴할 결과를 ref로 관리
+    const prevRef = useRef<{ props: P; result: React.ReactNode | null } | null>(null);
+
+    // 이전 프롭스와 현재 프롭스 비교
+    if (!prevRef.current || !equals(prevRef.current.props, props)) {
+      // 프롭스가 다르면 새로 렌더링
+      prevRef.current = { props, result: Component(props) as React.ReactNode };
+    }
+    // 프롭스가 같으면 이전 결과 재사용
+    return prevRef.current.result;
+  };
+  return MemoizedComponent;
 }
