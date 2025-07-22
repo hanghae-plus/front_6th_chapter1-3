1bcfd5f663211e4b249c03a882d45a6f83c8c6b1
soyalattee <uyyu1734@gmail.com>
2025-07-23 01:12:11 +0900

    feat:deepmemo 구현 => memo랑 비슷한방식인데 deepEqulas 사용

--- packages/lib/src/hocs/deepMemo.ts
+++ packages/lib/src/hocs/deepMemo.ts
@@ -1,5 +1,16 @@
 import type { FunctionComponent } from "react";
+import { useRef } from "../hooks";
+import { deepEquals } from "../equals/deepEquals";
 
 export function deepMemo<P extends object>(Component: FunctionComponent<P>) {
-  return Component;
+  const MemoizedComponent = (props: P) => {
+    const prevRef = useRef<P | null>(null);
+    if (!deepEquals(prevRef.current, props)) {
+      prevRef.current = props;
+      return Component(props);
+    }
+    return null;
+  };
+
+  return MemoizedComponent;
 }
