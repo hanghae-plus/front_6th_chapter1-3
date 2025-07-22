8aacb390aac0506f864fbb0e397b6103cd362b61
soyalattee <uyyu1734@gmail.com>
2025-07-23 01:11:21 +0900

    feat:memo 구현 => 메모이제이션컴포넌트를 내보낸다. 안에서는 useRef를 사용해 프로퍼티의 변경상태를 체크해서 Component를 내보내 재렌더링시키거나 null을 보냄(null을 보내면 렌더링이 안되는거 맞겠지..?)

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,19 @@
 import { type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  //2. MemoizedComponent
+  const MemoizedComponent = (props: P) => {
+    //1. prevRef
+    const prevRef = useRef<P | null>(null);
+
+    if (!equals(prevRef.current, props)) {
+      prevRef.current = props;
+      return Component(props);
+    }
+    return null;
+  };
+
+  return MemoizedComponent;
 }
