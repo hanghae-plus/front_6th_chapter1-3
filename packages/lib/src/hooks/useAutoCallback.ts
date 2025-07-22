7f0ca44410685ab16ad7fdc353d55f4b67ef80de
Amelia-Shin <gmldnjs0402@naver.com>
2025-07-23 01:18:01 +0900

    fix: useCallback 시 매개변수 전달

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -6,7 +6,7 @@ export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
   const ref = useRef<T>(fn);
 
   // 이후 렌더마다 최신값으로 업데이트
-  ref.current = fn;
+  ref.current = fn; 
 
-  return useCallback(() => ref.current(), []) as T;
-};
+  return useCallback(((...args: Parameters<T>): ReturnType<T> => ref.current(...args)) as T, []);
+}
