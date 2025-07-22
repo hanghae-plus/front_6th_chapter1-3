0ed217c1f93758094a0e32fd8a037665f9a9886f
Legitgoons <uichan@MacBook-Air-3.local>
2025-07-22 21:58:16 +0900

    feat: deepEquals 구현

--- packages/lib/src/equals/deepEquals.ts
+++ packages/lib/src/equals/deepEquals.ts
@@ -1,3 +1,24 @@
-export const deepEquals = (a: unknown, b: unknown) => {
-  return a === b;
+import { isObject } from "./util";
+
+/**
+ * 두 값의 깊은 비교 결과를 반환
+ * @param {unknown} a 비교할 첫 번째 값
+ * @param {unknown} b 비교할 두 번째 값
+ * @returns {boolean} 두 값의 깊은 비교 결과
+ */
+export const deepEquals = (a: unknown, b: unknown): boolean => {
+  // 1. 기본 타입이거나 null인 경우 처리
+  if (a === b) return true;
+
+  // 2. 둘 다 객체인 경우:
+  //    - 배열인지 확인
+  //    - 객체의 키 개수가 다른 경우 처리
+  //    - 재귀적으로 각 속성에 대해 deepEquals 호출
+  if (!isObject(a) || !isObject(b)) return false;
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+  if (keysA.length !== keysB.length) return false;
+
+  return keysA.every((key) => deepEquals(a[key], b[key]));
 };
