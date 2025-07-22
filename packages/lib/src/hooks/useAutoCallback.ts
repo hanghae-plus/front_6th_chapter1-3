3932be51ae2334ec76fca8801fda0bbf03448adf
soyalattee <uyyu1734@gmail.com>
2025-07-23 01:05:02 +0900

    feat:useAutoCallback 구현 => useRef 사용해서 렌더링시 최신 fn은 업데이트해주어 최신값을 보게함. useCallback 사용하여 함수의 참조는 고정함

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -3,5 +3,12 @@ import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
 
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const fnRef = useRef(fn);
+
+  // 항상 최신 함수
+  fnRef.current = fn;
+
+  return useCallback((...args: Parameters<T>) => {
+    return fnRef.current(...args);
+  }, []) as T;
 };
