c1e45f2de19e5817aef576fc388bddb1666ba41c
tksemf7410 <tksemf7410@gmail.com>
2025-07-23 03:12:21 +0900

    fix: useShallowState type 오류 개선

--- packages/lib/src/hooks/useShallowState.ts
+++ packages/lib/src/hooks/useShallowState.ts
@@ -2,12 +2,13 @@ import { useState } from "react";
 import { shallowEquals } from "../equals";
 import { useCallback } from "./useCallback";
 
-export const useShallowState = <T>(initialValue: Parameters<typeof useState<T>>[0]) => {
+type State<T extends typeof useState> = Parameters<T>[0];
+export const useShallowState = <T extends typeof useState>(initialValue: State<T>) => {
   // export const useShallowState = <T>(initialValue: T) => {
   // useState를 사용하여 상태를 관리하고, shallowEquals를 사용하여 상태 변경을 감지하는 훅을 구현합니다.
   const [state, _setState] = useState(initialValue);
 
-  const setStateWithShallowEquals = useCallback((value: Parameters<typeof useState<T>>[0]) => {
+  const setStateWithShallowEquals = useCallback((value: State<typeof useState>) => {
     if (!shallowEquals(state, value)) {
       _setState(value);
     }
