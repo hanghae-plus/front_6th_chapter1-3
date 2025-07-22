53e463a242766e39d41c98122db0360b16f2e7a7
soyalattee <uyyu1734@gmail.com>
2025-07-23 00:47:09 +0900

    feat:얕은 복사 구현

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,3 +1,26 @@
 export const shallowEquals = (a: unknown, b: unknown) => {
+  if (typeof a !== typeof b) return false;
+
+  // 객체 비교
+  if (typeof a === "object" && typeof b === "object" && a !== null && b !== null) {
+    const objA = a as Record<string, unknown>;
+    const objB = b as Record<string, unknown>;
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+    if (keysA.length !== keysB.length) return false;
+
+    return keysA.every((key) => {
+      if (key in objA && key in objB) {
+        return objA[key] === objB[key];
+      }
+      return false;
+    });
+  }
+
+  // 배열 비교
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length) return false;
+    return a.every((v, index) => v === b[index]);
+  }
   return a === b;
 };
