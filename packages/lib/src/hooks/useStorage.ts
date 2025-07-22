161466e7b006582aaff26ea3aaa96880a4496a33
areumH <dkfmal0109@gmail.com>
2025-07-23 01:31:06 +0900

    feat: useStorage, useStore 함수 작성 및 관련 함수 수정

--- packages/lib/src/hooks/useStorage.ts
+++ packages/lib/src/hooks/useStorage.ts
@@ -4,12 +4,5 @@ import type { createStorage } from "../createStorage";
 type Storage<T> = ReturnType<typeof createStorage<T>>;
 
 export const useStorage = <T>(storage: Storage<T>) => {
-  const subscribe = (listener: () => void) => {
-    storage.subscribe(listener);
-    return () => {};
-  };
-
-  const store = useSyncExternalStore(subscribe, storage.get);
-
-  return store;
+  return useSyncExternalStore(storage.subscribe, storage.get);
 };
