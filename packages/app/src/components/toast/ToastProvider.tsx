11b95e0805e290ff8699643b3f5eef2480448ba2
BBAK_JUN <wnsguddl789@gmail.com>
2025-07-23 20:13:44 +0900

    refactor(app): optimize toast provider performance
    - separate toast context into command and query contexts to prevent unnecessary re-renders
    - use useAutoCallback for memoized callback functions
    - improve component structure with useMemo for context values

--- packages/app/src/components/toast/ToastProvider.tsx
+++ packages/app/src/components/toast/ToastProvider.tsx
@@ -1,52 +1,59 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, memo, type PropsWithChildren, useContext, useReducer } from "react";
+import { createContext, memo, type PropsWithChildren, useContext, useMemo, useReducer } from "react";
 import { createPortal } from "react-dom";
 import { Toast } from "./Toast";
 import { createActions, initialState, toastReducer, type ToastType } from "./toastReducer";
 import { debounce } from "../../utils";
+import { useAutoCallback } from "@hanghae-plus/lib";
 
 type ShowToast = (message: string, type: ToastType) => void;
 type Hide = () => void;
 
-const ToastContext = createContext<{
-  message: string;
-  type: ToastType;
-  show: ShowToast;
-  hide: Hide;
-}>({
-  ...initialState,
-  show: () => null,
-  hide: () => null,
-});
+type ToastQuery = { message: string; type: ToastType };
+type ToastCommand = { show: ShowToast; hide: Hide };
+
+const ToastQueryContext = createContext<ToastQuery>({ ...initialState });
+const ToastCommandContext = createContext<ToastCommand>({ show: () => null, hide: () => null });
 
 const DEFAULT_DELAY = 3000;
 
-const useToastContext = () => useContext(ToastContext);
+const useToastQueryContext = () => useContext(ToastQueryContext);
+const useToastCommandContext = () => useContext(ToastCommandContext);
+
 export const useToastCommand = () => {
-  const { show, hide } = useToastContext();
+  const { show, hide } = useToastCommandContext();
   return { show, hide };
 };
 export const useToastState = () => {
-  const { message, type } = useToastContext();
+  const { message, type } = useToastQueryContext();
   return { message, type };
 };
 
 export const ToastProvider = memo(({ children }: PropsWithChildren) => {
-  const [state, dispatch] = useReducer(toastReducer, initialState);
+  const [queryValue, dispatch] = useReducer(toastReducer, initialState);
   const { show, hide } = createActions(dispatch);
-  const visible = state.message !== "";
+  const visible = queryValue.message !== "";
 
   const hideAfter = debounce(hide, DEFAULT_DELAY);
 
-  const showWithHide: ShowToast = (...args) => {
-    show(...args);
+  const memoizedShow = useAutoCallback((message: string, type: ToastType) => {
+    show(message, type);
     hideAfter();
-  };
+  });
+
+  const memoizedHide = useAutoCallback(hide);
+
+  const memoizedCommandValue = useMemo(
+    () => ({ show: memoizedShow, hide: memoizedHide }),
+    [memoizedShow, memoizedHide],
+  );
 
   return (
-    <ToastContext value={{ show: showWithHide, hide, ...state }}>
-      {children}
-      {visible && createPortal(<Toast />, document.body)}
-    </ToastContext>
+    <ToastCommandContext.Provider value={memoizedCommandValue}>
+      <ToastQueryContext.Provider value={queryValue}>
+        {children}
+        {visible && createPortal(<Toast />, document.body)}
+      </ToastQueryContext.Provider>
+    </ToastCommandContext.Provider>
   );
 });
