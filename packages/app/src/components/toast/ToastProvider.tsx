86d27394418cbc070c794355555deebdcad49028
annkimm <zizicoo208@naver.com>
2025-07-23 21:00:11 +0900

    feat: edit toast provider

--- packages/app/src/components/toast/ToastProvider.tsx
+++ packages/app/src/components/toast/ToastProvider.tsx
@@ -4,49 +4,40 @@ import { createPortal } from "react-dom";
 import { Toast } from "./Toast";
 import { createActions, initialState, toastReducer, type ToastType } from "./toastReducer";
 import { debounce } from "../../utils";
+import { useAutoCallback } from "@hanghae-plus/lib";
+import { useMemo } from "@hanghae-plus/lib/src/hooks";
 
 type ShowToast = (message: string, type: ToastType) => void;
 type Hide = () => void;
 
-const ToastContext = createContext<{
-  message: string;
-  type: ToastType;
-  show: ShowToast;
-  hide: Hide;
-}>({
-  ...initialState,
+const ToastStateContext = createContext<{ message: string; type: ToastType }>(initialState);
+const ToastActionContext = createContext<{ show: ShowToast; hide: Hide }>({
   show: () => null,
   hide: () => null,
 });
-
 const DEFAULT_DELAY = 3000;
 
-const useToastContext = () => useContext(ToastContext);
-export const useToastCommand = () => {
-  const { show, hide } = useToastContext();
-  return { show, hide };
-};
-export const useToastState = () => {
-  const { message, type } = useToastContext();
-  return { message, type };
-};
+export const useToastCommand = () => useContext(ToastActionContext);
+export const useToastState = () => useContext(ToastStateContext);
 
 export const ToastProvider = memo(({ children }: PropsWithChildren) => {
   const [state, dispatch] = useReducer(toastReducer, initialState);
-  const { show, hide } = createActions(dispatch);
+  const { show, hide } = useMemo(() => createActions(dispatch), [dispatch]);
   const visible = state.message !== "";
-
   const hideAfter = debounce(hide, DEFAULT_DELAY);
-
-  const showWithHide: ShowToast = (...args) => {
+  const showWithHide: ShowToast = useAutoCallback((...args) => {
     show(...args);
     hideAfter();
-  };
+  });
+  const states = useMemo(() => ({ message: state.message, type: state.type }), [state.message, state.type]);
+  const actions = useMemo(() => ({ show: showWithHide, hide }), [showWithHide, hide]);
 
   return (
-    <ToastContext value={{ show: showWithHide, hide, ...state }}>
-      {children}
-      {visible && createPortal(<Toast />, document.body)}
-    </ToastContext>
+    <ToastStateContext value={states}>
+      <ToastActionContext value={actions}>
+        {children}
+        {visible && createPortal(<Toast />, document.body)}
+      </ToastActionContext>
+    </ToastStateContext>
   );
 });
