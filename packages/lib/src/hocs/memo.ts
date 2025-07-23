1f94546c348814321bbaa719018f87a5554c1a4b
annkimm <zizicoo208@naver.com>
2025-07-23 20:58:47 +0900

    feat: add deepMemo, memo

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,23 @@
 import { type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  function MemoizedComponent(props: P) {
+    const propsRef = useRef(props);
+    const componentRef = useRef<React.ReactNode | Promise<React.ReactNode> | null>(null);
+
+    if (componentRef.current === null) {
+      componentRef.current = Component(props);
+    }
+
+    if (!equals(propsRef.current, props)) {
+      propsRef.current = props;
+      componentRef.current = Component(props);
+    }
+
+    return componentRef.current;
+  }
+
+  return MemoizedComponent;
 }
