de826638cb3a0fec0767937da27ea512cdda0c7d
Hwirin-Kim <kimhuirin@gmail.com>
2025-07-23 21:47:22 +0900

    feat : useStore 완성

--- packages/lib/src/hooks/useStore.ts
+++ packages/lib/src/hooks/useStore.ts
@@ -8,6 +8,18 @@ const defaultSelector = <T, S = T>(state: T) => state as unknown as S;
 
 export const useStore = <T, S = T>(store: Store<T>, selector: (state: T) => S = defaultSelector<T, S>) => {
   // useSyncExternalStore와 useShallowSelector를 사용해서 store의 상태를 구독하고 가져오는 훅을 구현해보세요.
+
+  // 셀렉터 함수를 최적화
   const shallowSelector = useShallowSelector(selector);
-  return shallowSelector(store.getState());
+
+  // 최적화 된 getSnapshot 함수
+  const getSnapshot = () => shallowSelector(store.getState());
+
+  // getSnapshot에 최적화된 selector 함수를 전달하여 불필요한 리렌더링 방지
+  const state = useSyncExternalStore((onStoreChange) => {
+    store.subscribe(onStoreChange);
+    return () => store.unsubscribe(onStoreChange);
+  }, getSnapshot);
+
+  return state;
 };
