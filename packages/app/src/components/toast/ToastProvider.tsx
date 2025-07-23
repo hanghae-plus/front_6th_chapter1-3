04856b02a00e48c738ef841f93e147fc5f7cdd5a
Hwirin-Kim <kimhuirin@gmail.com>
2025-07-23 23:11:07 +0900

    feat : toast 최적화

--- packages/app/src/components/toast/ToastProvider.tsx
+++ packages/app/src/components/toast/ToastProvider.tsx
@@ -4,49 +4,64 @@ import { createPortal } from "react-dom";
 import { Toast } from "./Toast";
 import { createActions, initialState, toastReducer, type ToastType } from "./toastReducer";
 import { debounce } from "../../utils";
+import { useCallback, useMemo } from "@hanghae-plus/lib/src/hooks";
 
 type ShowToast = (message: string, type: ToastType) => void;
 type Hide = () => void;
 
-const ToastContext = createContext<{
+// 상태 컨텍스트
+const ToastStateContext = createContext<{
   message: string;
   type: ToastType;
+}>({
+  ...initialState,
+});
+
+// 액션 컨텍스트
+const ToastActionContext = createContext<{
   show: ShowToast;
   hide: Hide;
 }>({
-  ...initialState,
   show: () => null,
   hide: () => null,
 });
 
 const DEFAULT_DELAY = 3000;
 
-const useToastContext = () => useContext(ToastContext);
-export const useToastCommand = () => {
-  const { show, hide } = useToastContext();
-  return { show, hide };
-};
-export const useToastState = () => {
-  const { message, type } = useToastContext();
-  return { message, type };
-};
+export const useToastCommand = () => useContext(ToastActionContext);
+export const useToastState = () => useContext(ToastStateContext);
 
 export const ToastProvider = memo(({ children }: PropsWithChildren) => {
   const [state, dispatch] = useReducer(toastReducer, initialState);
-  const { show, hide } = createActions(dispatch);
+
+  // 메모이제이션 처리
+  const { show, hide } = useMemo(() => createActions(dispatch), [dispatch]);
   const visible = state.message !== "";
 
-  const hideAfter = debounce(hide, DEFAULT_DELAY);
+  // 메모이제이션 처리
+  const hideAfter = useMemo(() => debounce(hide, DEFAULT_DELAY), [hide]);
 
-  const showWithHide: ShowToast = (...args) => {
-    show(...args);
-    hideAfter();
-  };
+  // 메모이제이션 처리
+  const showWithHide: ShowToast = useCallback(
+    (...args) => {
+      show(...args);
+      hideAfter();
+    },
+    [show, hideAfter],
+  );
+
+  // 상태만 메모이제이션 처리
+  const memoizedStateValue = useMemo(() => ({ message: state.message, type: state.type }), [state.message, state.type]);
+
+  // 액션만 메모이제이션 처리
+  const memoizedActionValue = useMemo(() => ({ show: showWithHide, hide }), [showWithHide, hide]);
 
   return (
-    <ToastContext value={{ show: showWithHide, hide, ...state }}>
-      {children}
-      {visible && createPortal(<Toast />, document.body)}
-    </ToastContext>
+    <ToastStateContext.Provider value={memoizedStateValue}>
+      <ToastActionContext.Provider value={memoizedActionValue}>
+        {children}
+        {visible && createPortal(<Toast />, document.body)}
+      </ToastActionContext.Provider>
+    </ToastStateContext.Provider>
   );
 });
