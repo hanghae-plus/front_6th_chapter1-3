d6506508ab992b484e8ba588b02ed8016b75bf02
yong <yong@thecodit.com>
2025-07-23 23:05:03 +0900

    feat: useStore 구현

--- packages/lib/src/hooks/useShallowSelector.ts
+++ packages/lib/src/hooks/useShallowSelector.ts
@@ -4,6 +4,15 @@ import { shallowEquals } from "../equals";
 type Selector<T, S = T> = (state: T) => S;
 
 export const useShallowSelector = <T, S = T>(selector: Selector<T, S>) => {
-  // 이전 상태를 저장하고, shallowEquals를 사용하여 상태가 변경되었는지 확인하는 훅을 구현합니다.
-  return (state: T): S => selector(state);
+  const prevResultRef = useRef<S | null>(null);
+
+  return (state: T): S => {
+    const currentResult = selector(state);
+    if (shallowEquals(prevResultRef.current, currentResult)) {
+      return prevResultRef.current!;
+    }
+
+    prevResultRef.current = currentResult;
+    return currentResult;
+  };
 };
--- packages/lib/src/hooks/useShallowState.ts
+++ packages/lib/src/hooks/useShallowState.ts
@@ -1,11 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useCallback, useState } from "react";
 import { shallowEquals } from "../equals";
 import { isFunction } from "../equals/utils";
 
-// useState를 사용하여 상태를 관리하고, shallowEquals를 사용하여 상태 변경을 감지하는 훅을 구현합니다.
-
 export const useShallowState = <T>(initialValue: T | (() => T)) => {
   const [state, setState] = useState<T>(initialValue);
+
   const setShallowState = useCallback((value: T | ((prev: T) => T)) => {
     const nextValue = isFunction(value) ? value(state) : value;
     if (shallowEquals(state, nextValue)) {
