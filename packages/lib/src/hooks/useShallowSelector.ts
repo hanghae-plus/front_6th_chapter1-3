51aea8d4d5d873a96f9b1d41745c0896df7f8114
annkimm <zizicoo208@naver.com>
2025-07-23 20:56:02 +0900

    feat: add useAutoCallback, useDeepMemo, useShallowSelector, useShallowState

--- packages/lib/src/hooks/useShallowSelector.ts
+++ packages/lib/src/hooks/useShallowSelector.ts
@@ -5,5 +5,14 @@ type Selector<T, S = T> = (state: T) => S;
 
 export const useShallowSelector = <T, S = T>(selector: Selector<T, S>) => {
   // 이전 상태를 저장하고, shallowEquals를 사용하여 상태가 변경되었는지 확인하는 훅을 구현합니다.
-  return (state: T): S => selector(state);
+
+  const prevRef = useRef<undefined | S>(undefined);
+
+  return (state: T): S => {
+    if (prevRef.current === undefined || !shallowEquals(prevRef.current, selector(state))) {
+      prevRef.current = selector(state);
+      return selector(state);
+    }
+    return prevRef.current;
+  };
 };
--- packages/lib/src/hooks/useShallowState.ts
+++ packages/lib/src/hooks/useShallowState.ts
@@ -1,7 +1,19 @@
 import { useState } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "./useRef";
+import { useCallback } from "./useCallback";
 
 export const useShallowState = <T>(initialValue: Parameters<typeof useState<T>>[0]) => {
-  // useState를 사용하여 상태를 관리하고, shallowEquals를 사용하여 상태 변경을 감지하는 훅을 구현합니다.
-  return useState(initialValue);
+  const prevRef = useRef<T | null>(initialValue ?? null);
+  const [state, setState] = useState(initialValue);
+
+  const setShallowState = useCallback((next: T) => {
+    // 얕은 비교: 다르면 실제로 setState 호출
+    if (!shallowEquals(prevRef.current, next)) {
+      setState(next as never);
+    }
+    prevRef.current = next;
+  }, []);
+
+  return [state, setShallowState] as const;
 };
