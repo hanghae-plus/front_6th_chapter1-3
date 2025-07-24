84eb21f390ce43390e5a2d3a38d566b1d1a38e1e
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-24 17:41:21 +0900

    refactor: useShallowState 에 useAutoCallback 적용

--- packages/lib/src/hooks/useShallowState.ts
+++ packages/lib/src/hooks/useShallowState.ts
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import { shallowEquals } from "../equals";
-import { useCallback } from "./useCallback";
+import { useAutoCallback } from "./useAutoCallback";
 
 type State<T extends typeof useState> = Parameters<T>[0];
 
 export const useShallowState = <T, S = State<typeof useState<T>>>(initialValue: S): [S, (newValue: S) => void] => {
   // useState를 사용하여 상태를 관리하고, shallowEquals를 사용하여 상태 변경을 감지하는 훅을 구현합니다.
   const [value, _setValue] = useState(initialValue);
-
-  const setValue = useCallback((newValue: S) => {
-    _setValue((prev) => (!shallowEquals(value, newValue) ? newValue : prev));
-  }, []);
+  const setValue = useAutoCallback((newValue: S) => {
+    _setValue((prev) => (shallowEquals(value, newValue) ? prev : newValue));
+  });
 
   return [value, setValue];
 };
