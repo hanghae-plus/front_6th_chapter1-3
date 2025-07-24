5db305f33d3c66121f4b15094c2f8fd702049ae5
JungWoo0203 <leejungwoo0203@gmail.com>
2025-07-24 20:57:46 +0900

    feat: shallowEquals, deepEquals, useRef 구현

--- packages/lib/src/equals/deepEquals.ts
+++ packages/lib/src/equals/deepEquals.ts
@@ -1,3 +1,19 @@
 export const deepEquals = (a: unknown, b: unknown) => {
-  return a === b;
+  if (a === b) return true;
+
+  if (typeof a !== "object" || a === null || typeof b !== "object" || b === null) return false;
+
+  if (Array.isArray(a) !== Array.isArray(b)) return false;
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) return false;
+
+  for (const key in a) {
+    if (Object.prototype.hasOwnProperty.call(a, key)) {
+      if (!deepEquals((a as Record<string, unknown>)[key], (b as Record<string, unknown>)[key])) return false;
+    }
+  }
+  return true;
 };
