477bcde7bbce9a4eb4a85e6369239110425f8013
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-07-24 22:01:48 +0900

    feat: memo, deepMemo 구현

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -3,10 +3,11 @@ import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
   const fnRef = useRef(fn);
-  // 갱신
+  // 갱신필요 => stale closure
   fnRef.current = fn;
   const stableFn = useCallback((...args: Parameters<T>) => {
     return fnRef.current(...args);
+    // 참조유지
   }, []);
 
   return stableFn as T;
