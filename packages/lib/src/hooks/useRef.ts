6d00097e83d1bc1d656b804fd43ea8c04f4f54b2
chan9yu <dev.cgyeo@gmail.com>
2025-07-25 01:45:11 +0900

    refactor: useRef 내부 useState 초기화 부분 lazy initialization 방식으로 개선

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -6,6 +6,6 @@ type RefObject<T> = {
 
 export function useRef<T>(initialValue: T) {
   // useState를 이용해서 리렌더링 간에 동일한 참조를 유지하는 ref 객체를 생성
-  const [refObject] = useState<RefObject<T>>({ current: initialValue });
+  const [refObject] = useState<RefObject<T>>(() => ({ current: initialValue }));
   return refObject;
 }
