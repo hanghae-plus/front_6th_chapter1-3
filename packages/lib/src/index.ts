7e81b01a8f700025fcce6776861991edf112ea45
devchaeyoung <chaemaa00@gmail.com>
2025-07-25 02:53:44 +0900

    fix : useMemo 커스텀훅으로 변경

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -2,7 +2,7 @@ export * from "./createObserver";
 export * from "./createStorage";
 export * from "./createStore";
 export * from "./Router";
-export { useStore, useStorage, useRouter, useAutoCallback, useCallback, useMemo } from "./hooks";
+export { useStore, useStorage, useRouter, useAutoCallback, useCallback, useMemo, useRef } from "./hooks";
 export * from "./equals";
 export * from "./types";
 export * from "./utils";
