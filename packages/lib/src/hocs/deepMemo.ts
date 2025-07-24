08a978cfc9a6b569bf1176991ba954336f7cc0a0
soyalattee <uyyu1734@gmail.com>
2025-07-25 02:14:59 +0900

    fix: deepmemo 수정 -> memo사용하도록 수정

--- packages/lib/src/hocs/deepMemo.ts
+++ packages/lib/src/hocs/deepMemo.ts
@@ -1,16 +1,7 @@
 import type { FunctionComponent } from "react";
-import { useRef } from "../hooks";
 import { deepEquals } from "../equals/deepEquals";
+import { memo } from "./memo";
 
 export function deepMemo<P extends object>(Component: FunctionComponent<P>) {
-  const MemoizedComponent = (props: P) => {
-    const prevRef = useRef<P | null>(null);
-    if (!deepEquals(prevRef.current, props)) {
-      prevRef.current = props;
-      return Component(props);
-    }
-    return null;
-  };
-
-  return MemoizedComponent;
+  return memo(Component, deepEquals);
 }
