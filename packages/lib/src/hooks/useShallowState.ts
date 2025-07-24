6bb1f8c4733e33000bbe8cc7114cb5d280654a5c
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-25 01:38:54 +0900

    refactor: useShallowState 수정

--- packages/lib/src/hooks/useShallowState.ts
+++ packages/lib/src/hooks/useShallowState.ts
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useState, type Dispatch, type SetStateAction } from "react";
 import { shallowEquals } from "../equals";
 import { useAutoCallback } from "./useAutoCallback";
 
-type State<T extends typeof useState> = Parameters<T>[0];
-
-export const useShallowState = <T, S = State<typeof useState<T>>>(initialValue: S): [S, (newValue: S) => void] => {
+export const useShallowState = <T>(initialValue: T): [T, Dispatch<SetStateAction<T>>] => {
   // useState를 사용하여 상태를 관리하고, shallowEquals를 사용하여 상태 변경을 감지하는 훅을 구현합니다.
   const [value, _setValue] = useState(initialValue);
-  const setValue = useAutoCallback((newValue: S) => {
-    _setValue((prev) => (shallowEquals(value, newValue) ? prev : newValue));
+  const setValue = useAutoCallback((value: SetStateAction<T>) => {
+    _setValue((prev) => {
+      const next = isFunction(value) ? value(prev) : value;
+      return shallowEquals(prev, next) ? prev : next;
+    });
   });
 
   return [value, setValue];
 };
+
+function isFunction<T>(value: SetStateAction<T>): value is (prev: T) => T {
+  return typeof value === "function";
+}
